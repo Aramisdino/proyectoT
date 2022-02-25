b98b93b3c47bd8b21c196ab245a539325fc488d4
Aramisdino <92555999+Aramisdino@users.noreply.github.com>
2022-02-24 21:08:51 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,22 +1,23 @@
 from django.db import models
+from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
 
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=60)
-    apellido = models.CharField(max_length=100)
+    raza = models.CharField(max_length=100)
     nacimiento = models.CharField(max_length=50,blank=True)
     edad = models.CharField(max_length=50)
     foto = models.ImageField(
         upload_to = 'foto/%Y/%m/%d',
         blank = True,
-        verbose_name = ('Foto de la persona')
+        verbose_name = ('Foto del animal')
     )
 
 class Meta:
-    verbose_name = ('Persona')
-    verbose_name_plural = ('Personas')
+    verbose_name = ('animal')
+    verbose_name_plural = ('animales')
 
 def __str__(self):
     return self.nombre
